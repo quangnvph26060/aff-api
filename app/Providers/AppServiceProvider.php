8e8f86c7986e3fc43437d2c4afed9e97641c2c79
quangnvph26060 <quangnvph26060@fpt.edu.vn>
2024-06-22 11:20:58 +0700

    wip update pusher oder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\View\Composers\OrderComposer;
 use App\Http\View\Composers\UserComposer;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         View::composer('*', UserComposer::class);
+        View::composer('*', OrderComposer::class);
     }
 }
