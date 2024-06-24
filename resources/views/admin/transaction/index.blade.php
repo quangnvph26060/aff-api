6232deb00bfa020c5f6371771a07a35aba509304
thangngo.dev <thangngo130802@gmail.com>
2024-06-24 16:32:40 +0700

    edit

--- resources/views/admin/transaction/index.blade.php
+++ resources/views/admin/transaction/index.blade.php
@@ -152,11 +152,12 @@
                         </div>
                         <div class="info-item">
                             <span class="label"><strong>Số tài khoản:</strong></span>
-                            <span class="value">{{ $item->user->user_info[0]->idnumber }}</span>
+                            <span class="value">{{ $item->user->user_info[0]->idnumber? $item->user->user_info[0]->idnumber : '' }}</span>
                         </div>
+                        <hr>
                         <div class="text-center">
-                            <p><strong>Mã QR:</strong></p>
-                            <img style="width:300px ; height: auto;" src="{{ $item->user->user_info[0]->branch }}"
+
+                            <img style="width:250px ; height: auto;" src="{{ $item->user->user_info[0]->branch?$item->user->user_info[0]->branch : '' }}"
                                 alt="QR Code" class="img-fluid">
                         </div>
                     </div>
