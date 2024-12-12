5174140f7de48af000378f49a4513b58a2537bde
quangnvph26060 <sgovietnam.dev@gmail.com>
2024-12-12 09:51:47 +0700

    update code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    {{-- <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> --}}
     <title>Affliate</title>
     <meta name="csrf-token" content={{ csrf_token() }}>
     @vite(['resources/js/aff-fe/src/main.js'])
